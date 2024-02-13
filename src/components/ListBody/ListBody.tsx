9cb85ada956cf92b3d8d00153cdbd1e4e7a5905d
Dream2000i <dream2000i@yandex.ru>
2024-02-13 20:21:51 +0300

    Завершена разработка приложения

--- src/components/ListBody/ListBody.tsx
+++ src/components/ListBody/ListBody.tsx
@@ -1,12 +1,6 @@
+import { Avatar, Checkbox } from 'antd';
 import { TypeListBodyProps } from '../../types/props';
-import { TypeUser } from '../../types/usersList';
 import './ListBody.scss';
-
-const get_avatar = ({ avatar, name }: TypeUser) => {
-    if (avatar) return <img src={avatar} alt='аватар' />;
-    const symbol = Array.from(name)[0];
-    return <div  className='img-placeholder'>{symbol}</div>;
-}
 
 
 export default function ListBody({ columns, data, handleChangeSelect }: TypeListBodyProps) {
@@ -14,12 +8,18 @@ export default function ListBody({ columns, data, handleChangeSelect }: TypeList
         <tbody className='users-list__body list-body'>
             {data.map((user) => (
                 <tr className={`list-body__row list-body__row ${String(user.selected)}`} key={`user-${user['id']}`} >
-                    <td className='list-body__col list-body__col_selected'><input type="checkbox" value={String(user.selected)} onChange={() => handleChangeSelect(user.id)} /></td>
+                    <td className='list-body__col list-body__col_selected'>
+                        <Checkbox checked={user.selected} onChange={() => handleChangeSelect(user.id)} />
+                    </td>
                     {columns.map(({ key }) => (
                         <td className={`list-body__col list-body__col_${key}`} key={`table-user-${key}`} >
-                            {
 
-                                key === 'avatar' ? get_avatar(user) : <div>{user[key]}</div>
+                            {
+                                key !== 'avatar'
+                                    ?
+                                    <div>{user[key]}</div>
+                                    :
+                                <Avatar src={user.avatar}>{Array.from(user.name)[0]}</Avatar>
                             }
                         </td>
                     ))}
